a6915493cbdc837087d9b834d26f9fc1983fa8f9
Maxine99 <65553632+Maxine99@users.noreply.github.com>
2020-05-18 17:41:26 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,108 @@
+<template>      
+    <div class="container">
+      <div class="head">
+        <h1 class="title">
+          Da Vinci Restaurant
+        </h1>
+        <h2>
+          Italienische Speisen von Pasta bis Pizza!
+        </h2>
+        <div class="links">
+          <nuxt-link
+            to="Speisekarte"
+            target="_blank"
+            class="button1"
+          >
+            Speisekarte
+          </nuxt-link>
+          <nuxt-link 
+            to="Historie"
+            target="_blank"
+            class="button1"
+          >
+            Historie unseres Restaurants
+          </nuxt-link>
+          <nuxt-link 
+            to="Gästebuch"
+            target="_blank"
+            class="button1"
+          >
+            Gästebuch
+          </nuxt-link>
+          <nuxt-link 
+            to="Kontakt"
+            target="_blank"
+            class="button1"
+          >
+            Kontakt
+          </nuxt-link>
+        </div>
+      </div>
+      <div class="inhalt">
+        <p>Wilkommen auf unserer Website!</p>
+        <img src="static/Banner.jpg"/>
+      </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.container {
+  background-color: #5F6A6A;
+  margin: 0 auto;
+  min-height: 100vh;
+  width: 100%;
+  position: absolute;
+  align-items: center;
+  text-align: center;
+}
+
+.head {
+  background-color: #424949;
+  height: 250px;
+  width: 100%;
+  position: absolute;
+  align-items: center;
+  text-align: center;
+}
+
+.inhalt{
+  background.color: #5F6A6A;
+  width: 100%;
+  height: 850px;
+
+  top: 250px;
+  position: absolute;
+
+  color: white;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #5DADE2;
+  letter-spacing: 1px;
+}
+
+img{
+  align: center;
+  width: 100%;
+  height: 100%;
+}
+
+h2
+{
+  color: white;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+</style>
